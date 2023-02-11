167456b209300b0ed9fe8b4ebf243a7f095e122b
AnneKinder <annekhd98@gmail.com>
2023-02-11 08:28:05 -0300

    feat: json_build_object

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -4,9 +4,33 @@ import { db } from "../database/database.connection.js";
 export async function getRentals(req, res){
 
     try{
-        const rentals = await db.query(`SELECT * FROM rentals`)
-
-        res.send(rentals.rows)
+        const rentals = await db.query(
+       `
+        SELECT json_build_object(
+            'customerId', rentals."customerId",
+            'gameId', rentals."gameId", 
+            'rentDate', rentals."rentDate",
+            'daysRented', rentals."daysRented",
+            'returnDate', rentals."returnDate",
+            'originalPrice', rentals."originalPrice",
+            'delayFee', rentals."delayFee",
+            'customer', json_build_object(
+                'id', customers.id,
+                'name', customers.name
+                ),
+            'game', json_build_object(
+                'id', games.id,
+                'name', games.name
+                )
+            )
+        FROM rentals 
+        JOIN customers
+        ON rentals."customerId"=customers.id
+        JOIN games
+        ON rentals."gameId"=games.id;
+        `)
+   
+       res.send(rentals.rows)
     }
     catch(err){
         res.status(500).send(err.message)
