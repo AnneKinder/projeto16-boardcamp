9d7890db082ac87f71dfab0dd9f4a89369b04bc9
AnneKinder <annekhd98@gmail.com>
2023-02-13 13:43:53 -0300

    fix: getRentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -4,31 +4,28 @@ import { db } from "../database/database.connection.js";
 export async function getRentals(req, res){
 
     try{
-        const rentals = await db.query(
-       `
-        SELECT json_build_object(
-            'customerId', rentals."customerId",
-            'gameId', rentals."gameId", 
-            'rentDate', rentals."rentDate",
-            'daysRented', rentals."daysRented",
-            'returnDate', rentals."returnDate",
-            'originalPrice', rentals."originalPrice",
-            'delayFee', rentals."delayFee",
-            'customer', json_build_object(
+         const rentals = await db.query(
+            `
+             SELECT rentals.*, json_build_object(
+               
                 'id', customers.id,
                 'name', customers.name
-                ),
-            'game', json_build_object(
-                'id', games.id,
-                'name', games.name
+                
                 )
-            )
-        FROM rentals 
-        JOIN customers
-        ON rentals."customerId"=customers.id
-        JOIN games
-        ON rentals."gameId"=games.id;
-        `)
+                AS customer, 
+                json_build_object(
+               
+                    'id', games.id,
+                    'name', games.name
+                    
+                    ) 
+                    AS game
+             FROM rentals 
+             JOIN customers
+             ON rentals."customerId"=customers.id
+             JOIN games
+             ON rentals."gameId"=games.id;
+             `)
    
        res.send(rentals.rows)
     }
